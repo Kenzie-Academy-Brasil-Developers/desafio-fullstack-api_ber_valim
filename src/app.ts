24129c867276c5c4b25dc58245b6a18e742cd5a4
bervalim <bernardogvalim@gmail.com>
2024-01-26 16:39:32 -0300

    feat/handle-error-middleware

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import "express-async-errors";
+import express, { Application } from "express";
+import { handleErrors } from "./middlewares/handleErrors.middleware";
+
+export const app: Application = express();
+
+app.use(express.json());
+
+app.use(handleErrors);
