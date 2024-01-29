098ec2a61a09a9334cfd6872ea702cba81217063
bervalim <bernardogvalim@gmail.com>
2024-01-29 10:55:05 -0300

    feat/TreadClientsList

--- src/interfaces/client.interface.ts
+++ src/interfaces/client.interface.ts
@@ -2,9 +2,14 @@ import { z } from "zod";
 import {
   clientWithoutAdminSchema,
   createClientRequestSchema,
+  clientResponseWithoutPasswordSchema,
 } from "../schemas/client.schema";
 
 export type TcreateClientRequest = z.infer<typeof createClientRequestSchema>;
 export type TclientWithoutAdmin = z.infer<typeof clientWithoutAdminSchema>;
 export type TupdateClientRequest = Partial<TclientWithoutAdmin>;
-// export type TclientResponseWithoutPassword
+export type TclientResponseWithoutPassword = z.infer<
+  typeof clientResponseWithoutPasswordSchema
+>;
+export type TreadClientsList = TclientResponseWithoutPassword[];
+export 
