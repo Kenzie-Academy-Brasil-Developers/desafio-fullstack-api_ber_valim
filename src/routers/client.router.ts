1258fcf8567aa92e0477fd7f18745b1b94db1d52
bervalim <bernardogvalim@gmail.com>
2024-01-29 14:55:13 -0300

    feat/validate-email

--- src/routers/client.router.ts
+++ src/routers/client.router.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { createClientController } from "../controller/client.controller";
 import { validateBody } from "../middlewares/validateBody.middleware";
 import { createClientRequestSchema } from "../schemas/client.schema";
+import { validateEmailExists } from "../middlewares/validateEmailExists.middleware";
 
 export const clientRouter: Router = Router();
 
 clientRouter.post(
   "/",
   validateBody(createClientRequestSchema),
+  validateEmailExists,
   createClientController
 );
