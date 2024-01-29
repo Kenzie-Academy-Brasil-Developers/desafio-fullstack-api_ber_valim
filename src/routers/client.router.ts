fdda64137fe598c3616f55ff37b8adcfe124bf81
bervalim <bernardogvalim@gmail.com>
2024-01-29 18:14:08 -0300

    test

--- src/routers/client.router.ts
+++ src/routers/client.router.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createClientController } from "../controller/client.controller";
+import {
+  createClientController,
+  readClientsController,
+} from "../controller/client.controller";
 import { validateBody } from "../middlewares/validateBody.middleware";
 import { createClientRequestSchema } from "../schemas/client.schema";
 import { validateEmailExists } from "../middlewares/validateEmailExists.middleware";
 import { validatePhoneExists } from "../middlewares/validatePhoneExists.middleware";
+import { verifyAdmin, verifyToken } from "../middlewares/security.middleware";
 
 export const clientRouter: Router = Router();
 
@@ -14,3 +18,4 @@ clientRouter.post(
   validatePhoneExists,
   createClientController
 );
+clientRouter.get("/", verifyToken, verifyAdmin, readClientsController);
