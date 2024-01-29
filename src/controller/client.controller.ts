7c4469a696403e650c64df0e56355985ba66edfb
bervalim <bernardogvalim@gmail.com>
2024-01-29 12:35:53 -0300

    feat/create-client-tested

--- src/controller/client.controller.ts
+++ src/controller/client.controller.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
-import { TclientResponseWithoutPassword } from "../interfaces/client.interface";
 import { createClientService } from "../services/client.service";
 
 export const createClientController = async (req: Request, res: Response) => {
-  const newClient = createClientService(req.body);
+  const newClient = await createClientService(req.body);
   return res.status(201).json(newClient);
 };
