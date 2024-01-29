c9f3435b420eaf0d38eaa36ace4e8631cbd4a5a0
bervalim <bernardogvalim@gmail.com>
2024-01-29 18:04:48 -0300

    feat/createClientController

--- src/controller/client.controller.ts
+++ src/controller/client.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
-import { createClientService } from "../services/client.service";
+import {
+  createClientService,
+  readClientsService,
+} from "../services/client.service";
 
 export const createClientController = async (
   req: Request,
@@ -7,4 +10,12 @@ export const createClientController = async (
 ): Promise<Response> => {
   const newClient = await createClientService(req.body);
   return res.status(201).json(newClient);
+};
+
+export const readClientsController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const clients = await readClientsService();
+  return res.status(200).json(clients);
 };
