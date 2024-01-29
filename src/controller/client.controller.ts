1571b39b3ab7516a6f828c4d32f9de970e4ea7ca
bervalim <bernardogvalim@gmail.com>
2024-01-29 16:01:39 -0300

    feat/session-finished

--- src/controller/client.controller.ts
+++ src/controller/client.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import { createClientService } from "../services/client.service";
 
-export const createClientController = async (req: Request, res: Response) => {
+export const createClientController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
   const newClient = await createClientService(req.body);
   return res.status(201).json(newClient);
 };
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { clientRouter } from "./client.router";
+import { sessionRouter } from "./session.router";
 
 export const allRoutes: Router = Router();
 
 allRoutes.use("/clients", clientRouter);
+allRoutes.use("/login", sessionRouter);
