203a79ea12d582c08b06ba83cd893d4b5a7271e0
bervalim <bernardogvalim@gmail.com>
2024-01-30 16:57:19 -0300

    feat/validateContactPhoneExists

--- src/middlewares/contact.middlewares.ts
+++ src/middlewares/contact.middlewares.ts
@@ -17,3 +17,37 @@ export const validateContactEmailExists = async (
 
   return next();
 };
+
+export const validateContactPhoneExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  if (!req.body.phone) return next();
+
+  const client = await prisma.contact.findUnique({
+    where: { phone: req.body.phone },
+  });
+
+  if (client) throw new AppError("Phone Already exists", 409);
+
+  return next();
+};
+
+export const verifyContactIdExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const { id } = req.params;
+
+  const contact = await prisma.contact.findUnique({
+    where: { id },
+  });
+
+  if (!contact) throw new AppError("Client not found", 404);
+
+  res.locals = { ...res.locals, contact };
+
+  return next();
+};
